7863ce680fff9c097112ab3a0622e741b9437328
waterfeeds <1006188571@qq.com>
2017-12-14 16:22:02 +0800

    init

---
+++ gproxy-common/src/main/java/com/waterfeeds/gproxy/cache/CacheFactory.java
@@ -0,0 +1,26 @@
+package com.waterfeeds.gproxy.cache;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CacheFactory<K, V> implements Cache<K, V> {
+    private ConcurrentHashMap<K, V> commons = new ConcurrentHashMap<K, V>();
+    @Override
+    public V getCache(K k) {
+        return commons.get(k);
+    }
+
+    @Override
+    public void addCache(K k, V v) {
+        commons.put(k, v);
+    }
+
+    @Override
+    public void putIfAbsentCache(K k, V v) {
+        commons.putIfAbsent(k, v);
+    }
+
+    @Override
+    public void remove(K k) {
+        commons.remove(k);
+    }
+}
---
+++ gproxy-proxy/src/main/java/com/waterfeeds/gproxy/banlance/Test.java
@@ -0,0 +1,7 @@
+package com.waterfeeds.gproxy.banlance;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
