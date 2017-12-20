dbd598bd80118d1cc4aa446f77b7556d42dd495b
waterfeeds <1006188571@qq.com>
2017-12-20 12:41:38 +0800

    add bit byte convert method

--- src/main/java/com/waterfeeds/gproxy/util/ByteUtil.java
+++ src/main/java/com/waterfeeds/gproxy/util/ByteUtil.java
@@ -25,13 +25,38 @@ public class ByteUtil {
     public static BitSet int2Bit(int num, int len) {
         BitSet bits = new BitSet(len);
         for (int i = 0; i < len; i++) {
-            //int offset = (len - 1 - i)
+            int offset = (len - 1 - i);
+            bits.set(i, ((num >>> offset) & 0x1) == 1);
         }
         return bits;
     }
 
-    public static int bit2Int(BitSet bits) {
-        return 0;
+    public static BitSet mergeBits(BitSet ...bits) {
+        int len = bits.length;
+        BitSet bitSets = new BitSet(len);
+        int index = 0;
+        for (BitSet bit: bits) {
+            int bLen = bit.length();
+            for (int i = 0; i < bLen; i++) {
+                bitSets.set(index ++, bit.get(i));
+            }
+        }
+        return bitSets;
+    }
+
+    public static byte[] bit2Byte(BitSet bits) {
+        int len = bits.length();
+        if (len % 8 != 0)
+            return new byte[0];
+        int bytesLen = len / 8;
+        byte[] bytes = new byte[bytesLen];
+        for (int i = 0; i < len; i = i + 8) {
+            for (int j = 0; j < 8; j++) {
+                int offset = 7 - j;
+                bytes[i / 8] |= ((bits.get(i + j) ? 0x1 : 0x0) & 0xFF) << offset;
+            }
+        }
+        return bytes;
     }
 
     public static void print(byte[] bytes) {
