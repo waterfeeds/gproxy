954718bcb491fc35d4ec0b0d148ea71bcb443d37
waterfeeds <1006188571@qq.com>
2017-12-20 20:18:39 +0800

    add proxy and server exception

--- src/main/java/com/waterfeeds/gproxy/proxy/exception/ProxyException.java
+++ src/main/java/com/waterfeeds/gproxy/proxy/exception/ProxyException.java
@@ -1,4 +1,14 @@
 package com.waterfeeds.gproxy.proxy.exception;
 
-public class ProxyException {
+public class ProxyException extends RuntimeException {
+    private int errorCode;
+
+    public ProxyException(String message) {
+        super(message);
+    }
+
+    public ProxyException(int errorCode, String message) {
+        super(errorCode + ": " + message);
+    }
 }
+
--- src/main/java/com/waterfeeds/gproxy/server/exception/ServerException.java
+++ src/main/java/com/waterfeeds/gproxy/server/exception/ServerException.java
@@ -1,4 +1,13 @@
 package com.waterfeeds.gproxy.server.exception;
 
-public class ServerException {
+public class ServerException extends RuntimeException {
+    private int errorCode;
+
+    public ServerException(String message) {
+        super(message);
+    }
+
+    public ServerException(int errorCode, String message) {
+        super(errorCode + ": " + message);
+    }
 }
