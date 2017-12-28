292c346dddfc633ea5091fc6dbb11441fdbd9961
waterfeeds <1006188571@qq.com>
2017-12-28 22:01:36 +0800

    add gproxy chinese coder

--- src/main/java/com/waterfeeds/gproxy/protocol/GproxyCoder.java
+++ src/main/java/com/waterfeeds/gproxy/protocol/GproxyCoder.java
@@ -1,10 +1,14 @@
 package com.waterfeeds.gproxy.protocol;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import com.waterfeeds.gproxy.protocol.base.AbstractGproxyCoder;
 import com.waterfeeds.gproxy.protocol.base.GproxyErrorCode;
 import com.waterfeeds.gproxy.protocol.exception.ProtocolException;
 import com.waterfeeds.gproxy.util.ByteUtil;
 import org.apache.commons.lang3.ArrayUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class GproxyCoder implements AbstractGproxyCoder {
     private GproxyCoder() {
@@ -24,12 +28,17 @@ public class GproxyCoder implements AbstractGproxyCoder {
         GproxyHeader header = protocol.getHeader();
         GproxyBody body = protocol.getBody();
         byte[] identifier = ByteUtil.int2Byte(protocol.getIdentifier(), 1);
-        byte[] length = ByteUtil.int2Byte(protocol.getLength(), 2);
         byte[] cmd = ByteUtil.int2Byte(header.getCmd(), 1);
         byte[] safe = ByteUtil.int2Byte(header.getSafe(), 1);
-        byte[] contentLen = ByteUtil.int2Byte(header.getContentLen(), 2);
         byte[] safeSign = body.getSafeSign().getBytes();
         byte[] content = body.getContent().getBytes();
+        byte[] contentLen = ByteUtil.int2Byte(content.length, 2);
+        byte[] length = null;
+        if (header.getSafe() == 0) {
+            length = ByteUtil.int2Byte(content.length + 4, 2);
+        } else {
+            length = ByteUtil.int2Byte(content.length + 20, 2);
+        }
         byte[] data = ByteUtil.mergeArrays(identifier, length, cmd, safe, contentLen, safeSign, content);
         return data;
     }
@@ -45,16 +54,32 @@ public class GproxyCoder implements AbstractGproxyCoder {
             if ((safe == 0 && bytes.length != contentLen + 4) || (safe == 1 && bytes.length != contentLen + 20)) {
                 throw new ProtocolException(GproxyErrorCode.BAD_PROTOCOL_CODE, GproxyErrorCode.BAD_PROTOCOL_MESSAGE);
             }
-            if (safe == 0) {
-                content = new String(ArrayUtils.subarray(bytes, 4, bytes.length));
-            } else {
-                safeSign = new String(ArrayUtils.subarray(bytes, 4, 20));
-                content = new String(ArrayUtils.subarray(bytes, 20, bytes.length));
+            try {
+                if (safe == 0) {
+                    content = new String(ArrayUtils.subarray(bytes, 4, bytes.length), "UTF-8");
+                } else {
+                    safeSign = new String(ArrayUtils.subarray(bytes, 4, 20));
+                    content = new String(ArrayUtils.subarray(bytes, 20, bytes.length), "UTF-8");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
         GproxyHeader header = new GproxyHeader(cmd, safe, contentLen);
         GproxyBody body = new GproxyBody(safeSign, content);
         return new GproxyProtocol(header, body);
+    }
+
+    public static void main(String[] args) {
+        byte[] content = null;
+        try {
+            int cmd = 101;
+            content = ByteUtil.mergeArrays(ByteUtil.int2Byte(cmd, 1), "高凯".getBytes("UTF-8"));
+            String data = new String(ArrayUtils.subarray(content, 1, content.length), "UTF-8");
+            System.out.println(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
